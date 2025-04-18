12c08118a8b5695ed29246e698d260279ae21924
Batyiluyolo <lloydbatyi101@gmail.com>
2025-04-18 02:10:33 +0200

    Update student.py

--- src/student.py
+++ src/student.py
@@ -1,1 +1,16 @@
 
+# src/student.py
+
+from user import User
+
+class Student(User):
+    def __init__(self, user_id, name, email, password, faculty, year):
+        super().__init__(user_id, name, email, password, role="Student")
+        self.faculty = faculty
+        self.year = year
+
+    def view_schedule(self):
+        print(f"{self.name} is viewing their schedule.")
+
+    def track_shuttle(self):
+        print(f"{self.name} is tracking a shuttle.")
