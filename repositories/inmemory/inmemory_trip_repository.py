b0caf51f4c813375134bb383d1ef5c1c6e553198
Batyiluyolo <lloydbatyi101@gmail.com>
2025-04-24 02:34:07 +0200

    Add files via upload

---
+++ repositories/inmemory/inmemory_trip_repository.py
@@ -0,0 +1,22 @@
+
+# repositories/inmemory/inmemory_trip_repository.py
+
+from repositories.trip_repository import TripRepository
+from src.trip import Trip
+
+class InMemoryTripRepository(TripRepository):
+    def __init__(self):
+        self._trips = {}
+
+    def save(self, trip: Trip) -> None:
+        self._trips[trip.trip_id] = trip
+
+    def find_by_id(self, trip_id: str) -> Trip:
+        return self._trips.get(trip_id)
+
+    def find_all(self) -> list[Trip]:
+        return list(self._trips.values())
+
+    def delete(self, trip: Trip) -> None:
+        if trip.trip_id in self._trips:
+            del self._trips[trip.trip_id]
