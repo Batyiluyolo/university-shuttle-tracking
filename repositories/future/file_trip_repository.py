267739f66808d4664567f8e659738ab3ac08bd8b
Batyiluyolo <lloydbatyi101@gmail.com>
2025-04-24 02:43:40 +0200

    Add files via upload

---
+++ repositories/future/file_trip_repository.py
@@ -0,0 +1,18 @@
+
+# repositories/future/file_trip_repository.py
+
+from repositories.trip_repository import TripRepository
+from src.trip import Trip
+
+class FileSystemTripRepository(TripRepository):
+    def save(self, trip: Trip) -> None:
+        raise NotImplementedError("This will save the trip to a file in the future.")
+
+    def find_by_id(self, trip_id: str) -> Trip:
+        raise NotImplementedError("This will retrieve the trip from a file in the future.")
+
+    def find_all(self) -> list[Trip]:
+        raise NotImplementedError("This will list all trips stored in files.")
+
+    def delete(self, trip: Trip) -> None:
+        raise NotImplementedError("This will delete a trip from the file system.")
